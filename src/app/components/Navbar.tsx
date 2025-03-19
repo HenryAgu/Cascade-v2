b8af1fb320f743291684e8c2bced15f69d3ae0ae
Henry Agu <aguhenrychuks@gmail.com>
2025-03-19 20:44:23 +0100

    fix

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='fixed top-0 left-0 right-0 border-b bg-white border-black'>
+    <nav className='fixed top-0 left-0 right-0 drop-shadow-md bg-white'>
         <div className="container mx-auto py-5 px-20">Dubai Navbar</div>
     </nav>
   )
