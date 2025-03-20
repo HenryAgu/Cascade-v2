50da1e5928428489cf7c74bae69cbd8bd9088bc9
Henry Agu <aguhenrychuks@gmail.com>
2025-03-20 13:36:00 +0100

    fix: route colot

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface NavMenu {
@@ -27,24 +29,39 @@ const navMenu: NavMenu[] = [
 ];
 
 const Navbar = () => {
+  const linkActive = usePathname();
   return (
     <nav className="fixed top-0 left-0 right-0 drop-shadow-md bg-white font-source">
       <div className="container mx-auto py-5 px-20 flex items-center justify-between">
-        <div className="">
+        <Link href="/" className="">
           <Image
             src="/images/Logo.svg"
             alt="Logo"
             width={132}
             height={32}
-            className="h-[32px] aspect-[132/32]"
+            className="h-[32px] w-fit aspect-[132/32]"
           />
-        </div>
+        </Link>
         <div className="flex items-center gap-x-10">
-          {navMenu.map((item, index) => (
-            <Link href={item.path} key={index} className="text-sm font-normal text-primary hover:text-brand-blue">
-              {item.title}
-            </Link>
-          ))}
+          <div className="flex items-center gap-x-10">
+            {navMenu.map((item, index) => (
+              <Link
+                href={item.path}
+                key={index}
+                className={`text-sm ${
+                  item.path === linkActive ? "text-[#0634e9] font-medium" : "text-[#5b6780] font-normal"
+                } hover:text-[#0634e9]`}
+              >
+                {item.title}
+              </Link>
+            ))}
+          </div>
+          <Link
+            href="/contact"
+            className="border border-[#0634e9] text-[#0056d2] text-sm font-medium rounded-md py-2 px-3"
+          >
+            Start Learning
+          </Link>
         </div>
       </div>
     </nav>
