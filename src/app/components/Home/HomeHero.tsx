f9c449b66b859e04ad79c68a0c51a4e059ece393
Henry Agu <aguhenrychuks@gmail.com>
2025-03-23 22:35:03 +0100

    feat: baseline

--- src/app/components/Home/HomeHero.tsx
+++ src/app/components/Home/HomeHero.tsx
@@ -7,14 +7,14 @@ const HomeHero = () => {
     <section className="flex lg:flex-row flex-col items-center gap-10 py-5 lg:py-10 font-source w-full">
       <div className="basis-full lg:basis-[50%] flex flex-col lg:text-left text-center gap-y-5 w-full">
         <h1 className="text-5xl text-black font-medium leading-[60px] hidden lg:block">
-          Empowering Tomorrow's <br /> Innovators
+          Empowering Tomorrow&lsquo;s <br /> Innovators
         </h1>
         <h1 className="text-[32px] leading-10 text-black font-medium block lg:hidden">
-        Empowering Tomorrow's Innovators 
+        Empowering Tomorrow&lsquo;s Innovators 
         </h1>
         <p className="text-base text-black max-w-full lg:max-w-[500px] font-normal">
           Welcome to our innovative platform, where we are dedicated to
-          empowering tomorrow &lsquo;,s innovators through early tech education for
+          empowering tomorrow &lsquo;s innovators through early tech education for
           young minds.
         </p>
         <div className="flex md:flex-row flex-col items-center gap-5 w-full lg:mt-5">
