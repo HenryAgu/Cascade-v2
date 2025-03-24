d3a9649ed141e06e28edc0b178bea0af6dbc13a6
Henry Agu <aguhenrychuks@gmail.com>
2025-03-24 21:05:47 +0100

    feat: new section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import NextStep from "../components/NextStep";
+import Banner from "../components/Banner";
 
 const AboutUsPage = () => {
   return (
     <main className="min-h-screen pt-20 lg:pt-28 bg-white text-black">
+        <Banner/>
       <NextStep />
     </main>
   );
