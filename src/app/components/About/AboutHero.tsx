ae19e1800842e1de302db781d6a231e5d7db5140
Henry Agu <aguhenrychuks@gmail.com>
2025-03-28 21:00:10 +0100

    feat: about page

--- src/app/components/About/AboutHero.tsx
+++ src/app/components/About/AboutHero.tsx
@@ -4,9 +4,9 @@ import React from "react";
 const AboutHero = () => {
   const heroImage = "/images/hero.jpg";
   return (
-    <section className="pb-5 lg:pb-24 w-full container mx-auto px-5 lg:px-20 flex gap-x-5">
-      <div className="basis-[50%] flex items-end px-10 py-20">
-        <p className="font-source text-[32px] font-bold leading-10 text-black2 max-w-[513px]">
+    <section className="pb-5 lg:pb-24 w-full container mx-auto px-0 lg:px-20 flex lg:flex-row flex-col-reverse gap-x-5">
+      <div className="basis-[50%] flex items-end p-5 py-10 lg:p-10">
+        <p className="font-source text-3xl lg:text-4xl font-semibold leading-11 text-black2 max-w-[513px]">
           We believe that everyone should
           have the chance to unlock their full potential and access endless
           opportunities.
@@ -19,7 +19,7 @@ const AboutHero = () => {
           aria-label="hero_image"
           width={816}
           height={740}
-          className="w-full h-[90vh] object-cover"
+          className="w-full h-screen lg:h-[90vh] object-cover"
         />
       </div>
     </section>
--- src/app/components/About/AboutSection.tsx
+++ src/app/components/About/AboutSection.tsx
@@ -40,10 +40,10 @@ const AboutSection = () => {
     <section className="py-5 lg:py-20 lg:pb-32 w-full container mx-auto px-5 lg:px-20">
       <div className="flex flex-col gap-y-24">
         <div className="flex flex-col gap-y-3.5 items-end justify-center">
-          <h3 className="text-3xl  font-source font-medium text-black">
+          <h3 className="text-2xl lg:text-3xl  font-source font-medium text-black">
             Who We Are
           </h3>
-          <p className="max-w-[520px] font-source text-lg text-black2 font-normal">
+          <p className="max-w-[520px] font-source text-base lg:text-lg text-black2 font-normal">
             Cascade was founded with a simple yet powerful idea: to create a
             world where growth, success, and access to opportunities are within
             reach for everyone. We are more than just a company—we are a
@@ -52,10 +52,10 @@ const AboutSection = () => {
           </p>
         </div>
         <div className="flex flex-col gap-y-3.5">
-          <h3 className="text-3xl  font-source font-medium text-black">
+          <h3 className="text-2xl lg:text-3xl  font-source font-medium text-black">
             What We Do
           </h3>
-          <p className="max-w-[520px] font-source text-lg text-black2 font-normal">
+          <p className="max-w-[520px] font-source text-base lg:text-lg text-black2 font-normal">
             We specialize in cutting-edge technology solutions Through
             innovation, dedication, and customer-centric solutions, we help
             individuals and businesses unlock their full potential. Our services
@@ -64,16 +64,16 @@ const AboutSection = () => {
           </p>
         </div>
         <div className="flex flex-col gap-y-3.5 items-end justify-center">
-          <h3 className="text-3xl  font-source font-medium text-black">
+          <h3 className="text-2xl lg:text-3xl  font-source font-medium text-black">
             Our Core Values
           </h3>
-          <span className=" font-source text-xl text-black2 font-normal">
+          <span className=" font-source text-base lg:text-xl text-black2 font-normal">
             At Cascade, we are guided by:
           </span>
           <ul className="flex flex-col gap-y-2 list-disc">
             {coreValues.map((item, index) => (
               <li
-                className="max-w-[520px] font-source text-lg text-black2 font-normal"
+                className="max-w-[520px] font-source text-base xl:text-lg text-black2 font-normal"
                 key={index}
               >
                 {`${item.header}: ${item.content}`}
@@ -88,7 +88,7 @@ const AboutSection = () => {
           alt="about_us_image"
           width={1170}
           height={600}
-          className="w-full h-[600px] object-cover rounded-md"
+          className="w-full h-[350px] lg:h-[600px] object-cover rounded-md"
         />
       </div>
     </section>
