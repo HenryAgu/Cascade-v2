c93f36b3b75ac0e576d12739329858693681c662
Henry Agu <aguhenrychuks@gmail.com>
2025-04-01 16:02:00 +0100

    vercel issues fixed

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -14,8 +14,8 @@ const ContactPage = () => {
             <p className="font-source font-normal text-xl lg:text-[32px] text-black2 max-w-[600px]">
               Ready to elevate your code base to new heights? Experience the
               exceptional expertise of a skilled mentor who effortlessly propels
-              projects to the next level. Let&lsquo;s partner with your needs to
-              maximize your code's potential. Don't miss out on this
+              projects to the next level. Let&apos;s partner with your needs to
+              maximize your code&apos;s potential. Don't miss out on this
               opportunity!
             </p>
           </div>
@@ -42,7 +42,7 @@ const ContactPage = () => {
                 className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium resize-none h-[180px] lg:h-[35vh]"
               />
               <button className="border flex items-center justify-between border-black2 text-left py-[14px] lg:py-[18px] px-5 max-w-full lg:max-w-[322px] rounded-full text-lg lg:text-[22px] font-source mt-5 lg:mt-10 cursor-pointer">
-                <span>Let's talk</span>
+                <span>Let&apos;s talk</span>
                 <MdArrowOutward className="text-2xl lg:text-3xl font-light"/>
               </button>
             </form>
