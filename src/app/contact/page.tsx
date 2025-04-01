6ebec8663e57685876aea34ca37eb00bf5be261e
Henry Agu <aguhenrychuks@gmail.com>
2025-04-01 12:22:36 +0100

    feat: contact banner

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NextStep from '../components/NextStep'
+import ContactBanner from '../components/ContactBanner'
 
 const ContactPage = () => {
   return (
     <div className="min-h-screen pt-20 lg:pt-28 bg-white text-black">
         <NextStep/>
+        <ContactBanner/>
     </div>
   )
 }
