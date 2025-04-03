6c1188244f53e655549c305738d84b1bd3bac79b
Henry Agu <aguhenrychuks@gmail.com>
2025-04-03 13:49:57 +0100

    state for form

---
+++ src/app/components/ContactForm.tsx
@@ -0,0 +1,39 @@
+"use client"
+import React, { useState } from 'react'
+import { MdArrowOutward } from 'react-icons/md'
+
+const ContactForm = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+  return (
+    <form action="" className="flex flex-col gap-y-1.5 lg:gap-y-5">
+    <input
+      type="text"
+      name="text"
+      id=""
+      placeholder="Your name here"
+      className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium"
+    />
+    <input
+      type="email"
+      name="email"
+      id=""
+      placeholder="Your email here"
+      className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium"
+    />
+    <textarea
+      name="message"
+      id=""
+      placeholder="Message"
+      className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium resize-none h-[180px] lg:h-[35vh]"
+    />
+    <button className="border flex items-center justify-between border-black2 text-left py-[14px] lg:py-[18px] px-5 max-w-full lg:max-w-[322px] rounded-full text-lg lg:text-[22px] font-source mt-5 lg:mt-10 cursor-pointer">
+      <span>Let&apos;s talk</span>
+      <MdArrowOutward className="text-2xl lg:text-3xl font-light" />
+    </button>
+  </form>
+  )
+}
+
+export default ContactForm
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ContactBanner from "../components/ContactBanner";
-import { MdArrowOutward } from "react-icons/md";
 import { Metadata } from "next";
+import ContactForm from "../components/ContactForm";
 
 export const metadata: Metadata = {
   title: "Contact Cascade | Empowering Learning Through Innovation",
@@ -60,32 +60,7 @@ const ContactPage = () => {
             </p>
           </div>
           <div className="basis-full lg:basis-[50%] w-full mb-14 lg:mb-0">
-            <form action="" className="flex flex-col gap-y-1.5 lg:gap-y-5">
-              <input
-                type="text"
-                name="text"
-                id=""
-                placeholder="Your name here"
-                className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium"
-              />
-              <input
-                type="email"
-                name="email"
-                id=""
-                placeholder="Your email here"
-                className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium"
-              />
-              <textarea
-                name="message"
-                id=""
-                placeholder="Message"
-                className="font-source border-b py-1.5 lg:py-3.5 border-black text-black outline-transparent mb-5 text-lg lg:text-2xl font-medium resize-none h-[180px] lg:h-[35vh]"
-              />
-              <button className="border flex items-center justify-between border-black2 text-left py-[14px] lg:py-[18px] px-5 max-w-full lg:max-w-[322px] rounded-full text-lg lg:text-[22px] font-source mt-5 lg:mt-10 cursor-pointer">
-                <span>Let&apos;s talk</span>
-                <MdArrowOutward className="text-2xl lg:text-3xl font-light" />
-              </button>
-            </form>
+            <ContactForm />
           </div>
         </div>
       </div>
