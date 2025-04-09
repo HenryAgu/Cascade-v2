410c4138d22976960981eaffa92c126792b4091d
Henry Agu <aguhenrychuks@gmail.com>
2025-04-09 12:24:06 +0100

    feat: test new

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const BlogPage = () => {
   return (
     <div className="min-h-screen pt-20 lg:pt-28 bg-white text-black flex items-center justify-center">
-      <h1 className="text-6xl lg:text-[90px] font-bold font-source animate-pulse text-center">Coming soon</h1>
+      <h1 className="text-6xl lg:text-[90px] font-bold font-source animate-pulse text-center">Coming soon Test</h1>
     </div>
   );
 };
