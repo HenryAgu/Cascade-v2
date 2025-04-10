ffa3a271533c949e8e01d12153c3e0d585da2873
Henry Agu <aguhenrychuks@gmail.com>
2025-04-10 09:33:28 +0100

    feat: new section

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -3,6 +3,7 @@ import BlogHero from "../components/Blog/BlogHero";
 import JustInBlogs from "../components/Blog/JustInBlogs";
 import TopPick from "../components/Blog/TopPick";
 import DailyColumn from "../components/Blog/DailyColumn";
+import ContactBanner from "../components/ContactBanner";
 
 const BlogPage = () => {
   return (
@@ -11,6 +12,7 @@ const BlogPage = () => {
       <JustInBlogs/>
       <TopPick/>
       <DailyColumn/>
+      <ContactBanner/>
     </main>
   );
 };
