955efe0467a8fa55834d836ddd04a84b02334bee
Henry Agu <aguhenrychuks@gmail.com>
2025-04-13 23:50:49 +0100

    feat: dynamic blog page

--- src/app/components/Blog/DailyColumn.tsx
+++ src/app/components/Blog/DailyColumn.tsx
@@ -1,81 +1,12 @@
+"use client"
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-interface JustIn {
-  image: string;
-  headline: string;
-  preview: string;
-  path: string;
-  author: string;
-}
 
-const justInBlogs: JustIn[] = [
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-  {
-    image: "/images/learning.jpg",
-    headline: "“I Am Seeing My Community of Researchers Decimated”",
-    path: "",
-    preview:
-      "Across the country, the Trump Administration’s assault on public institutions and its cuts to government funding are forcing scientists to abandon their work and the patients who benefit from it.",
-    author: "E. Tammy Kim",
-  },
-];
+import { useQuery } from "@tanstack/react-query";
+import { fetchBlog } from "../../../sanity/lib/fetchBlog";
+import { Spinner } from "../Spinner";
 
 const playfairDisplay = Playfair_Display({
   variable: "--font-playfairDisplay",
@@ -84,33 +15,46 @@ const playfairDisplay = Playfair_Display({
 });
 
 const DailyColumn = () => {
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ["blogs"], queryFn: fetchBlog });
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-fit">
+        <Spinner size={30} />
+      </div>
+    );
+  if (error) return <div> An error occured </div>;
   return (
     <section className="py-10 lg:py-20 px-5 lg:px-20 font-source">
       <p className="text-black2 font-bold uppercase text-2xl">Daily reads</p>
       <div className="py-7 flex gap-x-5 overflow-x-auto">
-        {justInBlogs.map((item, index) => (
+        {blogs?.slice(0, 4).map((item, index) => (
           <Link
-            href={`blog/${item.headline}`}
+            href={`/blog/${item?.slug?.current}`}
             className="flex flex-col gap-y-3.5 group shrink-0 max-w-full lg:max-w-[303px]"
             key={index}
           >
             <Image
-              src="/images/learning.jpg"
+              src={item?.mainImage?.asset?.url}
               height={220}
               width={444}
               className="w-full h-[220px]"
-              alt={`${item.headline}_image`}
+              alt={`${item?.title}_image`}
             />
             <h4
               className={`${playfairDisplay.className} line-clamp-2 text-black text-[22px] transition-all duration-200 ease-in-out leading-8 font-normal group-hover:underline`}
             >
-              {item.headline}
+              {item?.title}
             </h4>
             <p className="line-clamp-4 text-base font-normal text-black2">
-              {item.preview}
+              {item?.introduction}
             </p>
             <span className="font-bold text-base text-black">
-              By {item.author}
+              By {item?.author?.name}
             </span>
           </Link>
         ))}
--- src/app/components/Blog/TopPick.tsx
+++ src/app/components/Blog/TopPick.tsx
@@ -1,7 +1,11 @@
+"use client";
 import { Playfair_Display } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchBlog } from "../../../sanity/lib/fetchBlog";
 
 const playfairDisplay = Playfair_Display({
   variable: "--font-playfairDisplay",
@@ -10,28 +14,41 @@ const playfairDisplay = Playfair_Display({
 });
 
 const TopPick = () => {
-  const OurVisionImage = "/images/learning.jpg";
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useQuery({ queryKey: ["blogs"], queryFn: fetchBlog });
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-fit">Loading...</div>
+    );
+  if (error) return <div> An error occured </div>;
+
   return (
     <section className="py-10 lg:py-20 px-0 lg:px-14 xl:px-20 font-source flex lg:flex-row flex-col">
-      <Link href="" className="basis-[50%] bg-black text-white lg:px-20 lg:py-32 py-14 px-5 flex flex-col items-center justify-center  text-center gap-y-5 group">
-        <h6 className="text-sm font-normal tracking-wider">TOP PICK OF THE DAY</h6>
+      <Link
+        href={`/blog/${blogs?.[0]?.slug?.current}`}
+        className="basis-[50%] bg-black text-white lg:px-20 lg:py-32 py-14 px-5 flex flex-col items-center justify-center  text-center gap-y-5 group"
+      >
+        <h6 className="text-sm font-normal tracking-wider">
+          TOP PICK OF THE DAY
+        </h6>
         <h3
           className={`${playfairDisplay.className} text-3xl line-clamp-3 lg:text-[36px] font-normal group-hover:underline transition-all duration-200 ease-in-out`}
         >
-          I Am Seeing My Community of Researchers Decimated
+          {blogs?.[0]?.title}
         </h3>
         <p className="line-clamp-4 text-base lg:text-lg font-normal">
-          Across the country, the Trump Administration’s assault on public
-          institutions and its cuts to government funding are forcing scientists
-          to abandon their work and the patients who benefit from it.
+          {blogs?.[0]?.introduction}
         </p>
         <span className="line-clamp-4 text-base lg:text-lg font-normal">
-          By Henry Agu
+          By {blogs?.[0]?.author?.name}
         </span>
       </Link>
       <div className="basis-[50%]">
         <Image
-          src={OurVisionImage}
+          src={blogs?.[0]?.mainImage?.asset?.url ?? ""}
           alt="top_pick_image"
           width={680}
           height={648}
